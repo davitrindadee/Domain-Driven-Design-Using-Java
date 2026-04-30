f221464ef48984f7a88689a9b52490c11412d867
davitrindadee <davitr07@gmail.com>
2026-04-29 22:46:43 -0300

    Aprofundamento em Exemplos de Strings.

---
+++ Apostila08/src/br/com/fiap/apostila08/view/TesteString2.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apostila08.view;
+
+import javax.swing.*;
+
+public class TesteString2 {
+
+    public static void main(String[] args){
+        String nome = JOptionPane.showInputDialog("Digite o email");
+
+        //Validar se é um e-mail válido (possui @ e .)
+
+    }
+}
